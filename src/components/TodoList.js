4b9d48d491aec8fada8fffe2d9d3d838c36fa6a5
chris-wamae <chriswamae123@gmail.com>
2023-01-11 10:40:04 +0300

    Added Home component to App to be rendered in the DOM

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,10 +2,10 @@ import Home from "../view/Home"
 import TodoItem from "./TodoItem"
 function TodoList(){
     return (
-        <div>
+        <>
         <TodoItem />
         <TodoItem />
-        </div>
+        </>
     )
 }
 export default TodoList
